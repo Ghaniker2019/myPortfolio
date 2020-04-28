a79d1128c85a462a4bc605ba87e03faeb5e46420
kerkoub <kerkoub.abdelghani@gmail.com>
2020-04-28 08:57:37 +0200

    Updates

---
+++ precache-manifest.a013d5a8a1a74bf48f24ca22378bb795.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a6ea3355e653c2f5a96b2783189a8b",
+    "url": "/myPorfolio/index.html"
+  },
+  {
+    "revision": "26fe2eb1fef61b65bc8d",
+    "url": "/myPorfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "14716a8b27ca290cf49b",
+    "url": "/myPorfolio/static/css/main.ad37238c.chunk.css"
+  },
+  {
+    "revision": "26fe2eb1fef61b65bc8d",
+    "url": "/myPorfolio/static/js/2.e509a498.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myPorfolio/static/js/2.e509a498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14716a8b27ca290cf49b",
+    "url": "/myPorfolio/static/js/main.4792ead5.chunk.js"
+  },
+  {
+    "revision": "2ed18b0859d9d70c99b8",
+    "url": "/myPorfolio/static/js/runtime-main.6eab2ac1.js"
+  },
+  {
+    "revision": "13a5db43a20c198d2864c14865e7687f",
+    "url": "/myPorfolio/static/media/Compétences.13a5db43.png"
+  },
+  {
+    "revision": "561ebe82a3131b62ecec60f94905ddad",
+    "url": "/myPorfolio/static/media/PhotoGhani.561ebe82.jpg"
+  },
+  {
+    "revision": "cadcb13e36e380a9be856aa3059cb2c1",
+    "url": "/myPorfolio/static/media/allocine.cadcb13e.png"
+  },
+  {
+    "revision": "63c23bb3e21b8691983e99385a7e831b",
+    "url": "/myPorfolio/static/media/myTrainline.63c23bb3.png"
+  }
+]);
