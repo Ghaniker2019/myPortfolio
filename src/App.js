0592c70bf8fd3391525a8e5a9ee9ef8d031c80b0
kerkoub <kerkoub.abdelghani@gmail.com>
2020-04-28 10:37:36 +0200

    ;

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Fouter from "./Fouter";
 import Sections from "./Sections";
-import "./fonts/SourceSansPro-Regular.ttf";
 
 function App() {
   return (
