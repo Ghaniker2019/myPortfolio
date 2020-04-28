061e6ddb33a17e9633bc46b022d93e142dc46d63
kerkoub <kerkoub.abdelghani@gmail.com>
2020-04-28 09:45:42 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./sections.css";
+import "./app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
