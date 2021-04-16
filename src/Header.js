6b169a52324fdb2af1d7b3b9afc4f20758e21f91
kerkoub <kerkoub.abdelghani@gmail.com>
2021-04-16 14:02:56 +0200

    added Buttons link

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,27 @@ import "./header.css";
 function Header() {
   return (
     <div className="main_container">
+      <a href="https://github.com/Ghaniker2019"
+        target="_blank" rel="noopener noreferrer" > <img
+          className="imgLinkGit"
+          src={require("./img/github.png")}
+          alt="ma_photo"
+
+        /></a>
+      <a href="https://www.linkedin.com/in/kerkoub/"
+        target="_blank" rel="noopener noreferrer" >   <img
+          className="imgLinkLinkedin"
+          src={require("./img/linkedin.png")}
+          alt="ma_photo"
+
+        /></a>
+      <a href="https://www.instagram.com/ghanikerk/"
+        target="_blank" rel="noopener noreferrer" >    <img
+          className="imgLinkInsta"
+          src={require("./img/insta.png")}
+          alt="ma_photo"
+
+        /></a>
       <div className="container">
         <img
           className="img"
@@ -16,7 +37,7 @@ function Header() {
         <h1 className="title">Mon Portfolio</h1>
         <h2>Développeur web & mobile</h2>
       </div>
-    </div>
+    </div >
   );
 }
 
